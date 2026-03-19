251becf5a3812795d6c21bf0f3b73f838bab9a32
Zeinab Ghamlouch <zeinab.b.ghamloush@gmail.com>
2026-03-19 11:47:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual';
+}
+
+window.addEventListener("load", () => {
+  window.scrollTo(0, 0);
+});
+
 const reveals = document.querySelectorAll(".reveal");
 
 const observer = new IntersectionObserver(
